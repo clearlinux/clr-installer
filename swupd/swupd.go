cceb3db018b8b163d091535005cc1dc36c0b6dea
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2018-10-24 16:12:14 -0700

    swupd: use --keepcache with "update" command
    If we don't explicitly specify the --keepcache flag the previously
    fetch content (specially when using --swupd-state flag) will be
    cleaned up and all the content will be downloaded all over again.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

--- swupd/swupd.go
+++ swupd/swupd.go
@@ -128,6 +128,7 @@ func (s *SoftwareUpdater) Update() error {
 	args := []string{
 		filepath.Join(s.rootDir, "/usr/bin/swupd"),
 		"update",
+		"--keepcache",
 		fmt.Sprintf("--path=%s", s.rootDir),
 		fmt.Sprintf("--statedir=%s", s.stateDir),
 	}
