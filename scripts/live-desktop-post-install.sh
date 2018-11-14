4c5dcd9cefcc8732dd10e50252510e14811f0d1d
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2018-11-13 16:16:38 -0800

    live-desktop: don't install desktop and wrapper
    The .desktop and the desktop wrapper script are now installed by the
    make install target, so we don't need it anymore.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

--- scripts/live-desktop-post-install.sh
+++ scripts/live-desktop-post-install.sh
@@ -7,6 +7,7 @@ CHROOTPATH=$1
 GDM_DIR=$CHROOTPATH/etc/gdm/
 THEMES_DIR=$CHROOTPATH/usr/share/clr-installer/themes
 DESKTOP_DIR=$CHROOTPATH/usr/share/applications/
+VAR_DIR=$CHROOTPATH/var/lib/clr-installer
 
 usermod --root $CHROOTPATH -a -G wheelnopw clrlinux
 usermod --root $CHROOTPATH -u 1001 clrlinux
@@ -16,10 +17,8 @@ systemctl --root=$CHROOTPATH disable clr-installer
 systemd-machine-id-setup --root=$CHROOTPATH
 
 install -D -m 644 themes/clr.png $THEMES_DIR/clr.png
-install -D -m 644 etc/clr-installer.desktop $DESKTOP_DIR/clr-installer.desktop
 install -D -m 644 etc/custom.conf $GDM_DIR/custom.conf
-install -D -m 755 scripts/clr-installer-desktop.sh $CHROOTPATH/usr/bin/clr-installer-desktop.sh
-install -D -m 644 etc/clr-desktop.yaml $CHROOTPATH/var/lib/clr-installer/clr-installer.yaml
+install -D -m 644 etc/clr-desktop.yaml $VAR_DIR/clr-installer.yaml
 
 FAVORITE_APPS="['clr-installer.desktop', 'org.gnome.Terminal.desktop', \
        'org.gnome.Nautilus.desktop', 'firefox.desktop', \
