589c818b7a9aa4fd98a96908bf65088acac5473c
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2018-11-16 10:48:46 -0800

    Revert "window: fix event processing"
    This reverts commit 70b6c4fd0459211d408460f3eef82a8666ed245e.
    
    This change was merged upstream into clui and will be added in the
    upcoming revendoring patch.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

--- vendor/github.com/VladimirMarkelov/clui/window.go
+++ vendor/github.com/VladimirMarkelov/clui/window.go
@@ -251,7 +251,7 @@ func (c *Window) ProcessEvent(ev Event) bool {
 
 			if aC != nil && aC.Clipped() {
 				clipped = aC
-			} else if nC != nil {
+			} else {
 				clipped = ClippedParent(nC)
 			}
 
