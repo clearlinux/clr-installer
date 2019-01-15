5ea5cb0e67f7f9d5e3e8553bb143f2dff3ed9908
Ikey Doherty <ikey.doherty@intel.com>
2019-01-15 13:23:52 +0000

    Add new pkg github.com/gotk3/gotk3
    Add new vendor package
    	make vendor-add GOADD=github.com/gotk3/gotk3
    and the required dependencies.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@intel.com>

---
+++ vendor/github.com/gotk3/gotk3/glib/gmain_context.go
@@ -0,0 +1,31 @@
+package glib
+
+// #include <gio/gio.h>
+// #include <glib.h>
+// #include <glib-object.h>
+// #include "glib.go.h"
+import "C"
+
+type MainContext C.GMainContext
+
+// native returns a pointer to the underlying GMainContext.
+func (v *MainContext) native() *C.GMainContext {
+	if v == nil {
+		return nil
+	}
+	return (*C.GMainContext)(v)
+}
+
+// MainContextDefault is a wrapper around g_main_context_default().
+func MainContextDefault() *MainContext {
+	c := C.g_main_context_default()
+	if c == nil {
+		return nil
+	}
+	return (*MainContext)(c)
+}
+
+// MainDepth is a wrapper around g_main_depth().
+func MainDepth() int {
+	return int(C.g_main_depth())
+}
