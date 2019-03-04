46f33d9a019fcf9d6e27aa9d17c6f690fda1688d
Mark D Horn <mark.d.horn@intel.com>
2019-03-04 11:23:03 -0800

    New telemetry defaults
    Update the latest release telemetry configuration file
    and update the post install hook with the new default
    and new maximum.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/local-telemetry-post.sh
+++ scripts/local-telemetry-post.sh
@@ -8,7 +8,7 @@ cp $1/usr/share/defaults/telemetrics/telemetrics.conf \
    $1/etc/telemetrics/telemetrics.conf
 
 sed -i -e '/server=/s/clr.telemetry.intel.com/localhost/' \
-    -e '/spool_process_time/s/=900/=3600/' \
+    -e '/spool_process_time/s/=120/=300/' \
     -e '/record_retention_enabled/s/=false/=true/' \
     $1/etc/telemetrics/telemetrics.conf
 
