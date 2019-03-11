d6a2027f25be7bb0e9d759688046f258cb5a6996
Mark D Horn <mark.d.horn@intel.com>
2019-03-11 15:37:34 -0700

    Split the GUI and TUI
    Build two different binaries for the TUI and GUI so
    that we do not need to pull along overhead of the GUI
    for installations without a graphics environment.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

---
+++ clr-installer/tui.go
@@ -0,0 +1,21 @@
+// +build !guiBuild
+
+// Copyright © 2018 Intel Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+package main
+
+import (
+	"github.com/clearlinux/clr-installer/frontend"
+	"github.com/clearlinux/clr-installer/massinstall"
+	"github.com/clearlinux/clr-installer/tui"
+)
+
+// The list of possible frontends to run for TUI
+func initFrontendList() {
+	frontEndImpls = []frontend.Frontend{
+		massinstall.New(),
+		tui.New(),
+	}
+}
