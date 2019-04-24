6a94aab6e0b902daeca5b417a01f1aace901f0bb
Brian J Lovin <brian.j.lovin@intel.com>
2019-04-24 20:52:13 +0000

    Add boot message, disable autolaunch, change PS1
    Add a message printed on boot that gives basic information and
    instructions to login as root and run clr-installer to install onto this
    sytem.
    
    Also change $PS1 on the installer image after logged in to
    indicate we're booted into a live image.
    
    Remove clr-installer.service from the Makefile and remove all the
    toggling on or off from the yaml files.
    
    Signed-off-by: Brian J Lovin <brian.j.lovin@intel.com>

--- scripts/installer-post.sh
+++ scripts/installer-post.sh
@@ -8,4 +8,10 @@ scripts/local-telemetry-post.sh ${CHROOTPATH}
 # Delay booting to give user a change to change boot params
 scripts/wait-to-boot-post.sh ${CHROOTPATH}
 
+# Add issue (pre-login message) to inform user of how to run the installer
+scripts/add-login-issue.sh ${CHROOTPATH}
+
+# Add changes to PS1 to indicate live image
+scripts/ps1-override.sh ${CHROOTPATH}
+
 exit 0
