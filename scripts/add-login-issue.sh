2134b1834fd08e227f171b19a0c66e5df5e1c5e2
Mark D Horn <mark.d.horn@intel.com>
2019-04-29 11:17:52 -0700

    server: Convey how to configure network for server
    TODO: Remove the TUI networking support for setting
    static IP, possibly Proxies.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/add-login-issue.sh
+++ scripts/add-login-issue.sh
@@ -6,9 +6,12 @@ echo "Welcome to the Clear Linux* OS live image!
  * Documentation:     https://clearlinux.org/documentation
  * Community Support: https://clearlinux.org/community
 
-To install Clear Linux* OS onto this sytem please login as root,
+To configure the network run:
+  nmtui
+
+To install Clear Linux* OS onto this system please login as root,
 enter a new temporary password, and run:
-clr-installer
+  clr-installer
 " >> $1/etc/issue
 
 exit 0
