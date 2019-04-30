3ba2a9ec236ec1218408bf25d51184dda5005553
Mark D Horn <mark.d.horn@intel.com>
2019-04-30 10:10:53 -0700

    scripts: Add missing script and update sytle
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/developer-image-pre.sh
+++ scripts/developer-image-pre.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t
+# vi: set shiftwidth=4 tabstop=4 noexpandtab:
+# :indentSize=4:tabSize=4:noTabs=false:
+
+# Developer Image Pre Install steps
+
+CHROOTPATH=$1
+export HOOKDIR=$(dirname $0)
+
 LINES=$(git status --porcelain --untracked=no | wc -l)
 
 if [ ${LINES} -ne 0 ]; then
@@ -10,5 +19,16 @@ if [ ${LINES} -ne 0 ]; then
     exit 1
 fi
 
-
 exit 0
+
+# Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+#
+# Local variables:
+# c-basic-offset: 4
+# tab-width: 4
+# indent-tabs-mode: t
+# End:
+#
+# vi: set shiftwidth=4 tabstop=4 noexpandtab:
+# :indentSize=4:tabSize=4:noTabs=false:
+#
