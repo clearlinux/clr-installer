500a2957f7b3f296d1f002a4d0a8053ca6bbbdc9
Mark D Horn <mark.d.horn@intel.com>
2019-05-06 10:41:42 -0700

    scripts: set prompt via hostname
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/ps1-override.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Setting custom PS1 terminal prompt $1"
-
-echo "PS1='\[\e[38;5;39m\]\u\[\e[0m\]@\[\e[38;5;208m\]clr-live \[\e[38;5;39m\]\w \[\e[38;5;39m\]# \[\e[0;0m\]'" >> $1/etc/profile
-
-exit 0
