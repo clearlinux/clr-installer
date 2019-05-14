65a01e46bfcd9a5f112029ca3180d1d0900991ec
Mark D Horn <mark.d.horn@intel.com>
2019-05-13 17:27:31 -0700

    scripts: Keep proxies for root
    Since the installer runs as root, we need to ensure that the
    proxy environment variables are maintained.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/installer-post.sh
+++ scripts/installer-post.sh
@@ -14,4 +14,9 @@ scripts/add-login-issue.sh ${CHROOTPATH}
 # Add changes to PS1 to indicate live image by setting the hostname
 echo "clr-live" > ${CHROOTPATH}/etc/hostname
 
+# Allow proxy environment variables to be used by root for the install
+mkdir -p ${CHROOTPATH}/etc/sudoers.d/
+echo 'Defaults env_keep += "http_proxy https_proxy ftp_proxy no_proxy socks_proxy"'  >> ${CHROOTPATH}/etc/sudoers.d/env_keep
+echo 'Defaults env_keep += "HTTP_PROXY HTTPS_PROXY FTP_PROXY NO_PROXY SOCKS_PROXY"'  >> ${CHROOTPATH}/etc/sudoers.d/env_keep
+
 exit 0
