98089cc36dd43e8a11e1304fa3f952398e6a600b
Mark D Horn <mark.d.horn@intel.com>
2019-08-14 17:54:03 -0700

    scripts: sync with devops
    Pull in some recent devop changes so the developer test
    builds are in-sync.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

---
+++ scripts/add-desktop-login-issue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Creating custom issue file for $1"
+
+echo "Welcome to the Clear Linux* OS live image!
+
+ * Documentation:     https://clearlinux.org/documentation
+ * Community Support: https://clearlinux.org/community
+
+To install Clear Linux* OS onto this system please login as 'clrlinux',
+which has no password, and run:
+  sudo -E clr-installer
+
+" >> $1/etc/issue
+
+exit 0
