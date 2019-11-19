87345a307ba894cea2b47cb5f61778290b1b84c9
Mark D Horn <mark.d.horn@intel.com>
2019-11-18 17:52:12 -0800

    scripts: Community link moved
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- scripts/add-desktop-login-issue.sh
+++ scripts/add-desktop-login-issue.sh
@@ -4,7 +4,7 @@ echo "Creating custom issue file for $1"
 echo "Welcome to the Clear Linux* OS live image!
 
  * Documentation:     https://clearlinux.org/documentation
- * Community Support: https://clearlinux.org/community
+ * Community Support: https://community.clearlinux.org
 
 To install Clear Linux* OS onto this system please login as 'clrlinux',
 which has no password, and run:
