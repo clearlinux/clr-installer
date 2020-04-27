c004b7635d5b9e6bcfd2d257cd2775b86b9dc20e
Mark D Horn <mark.d.horn@intel.com>
2020-04-27 15:44:33 -0700

    storage: make undetermined guid warning only
    We were wrapping this in a error with trace back data
    which puts a misleading message into the log file.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- storage/block_devices_ops.go
+++ storage/block_devices_ops.go
@@ -234,20 +234,20 @@ func (bd *BlockDevice) updatePartitionInfo() error {
 //   + mount point
 //   + file system type (i.e swap)
 //   + or if it's the "special" efi case
-func (bd *BlockDevice) getGUID() (string, error) {
+func (bd *BlockDevice) getGUID() string {
 	if guid, ok := guidMap[bd.MountPoint]; ok {
-		return guid, nil
+		return guid
 	}
 
 	if guid, ok := guidMap[bd.FsType]; ok {
-		return guid, nil
+		return guid
 	}
 
 	if bd.FsType == "vfat" && bd.MountPoint == "/boot" {
-		return guidMap["efi"], nil
+		return guidMap["efi"]
 	}
 
-	return "none", errors.Errorf("Could not determine the guid for: %s", bd.Name)
+	return ""
 }
 
 func (bd *BlockDevice) isStandardMount() bool {
@@ -1122,9 +1122,10 @@ func (bd *BlockDevice) WritePartitionTable(wholeDisk bool, forceDestructive bool
 		// and we know their assigned numbers ...
 		for _, curr := range bd.Children {
 			var guid string
-			guid, err = curr.getGUID()
-			if err != nil {
-				log.Warning("%s", err)
+			guid = curr.getGUID()
+			if guid == "" {
+				log.Warning("Could not determine the guid for: %s", curr.Name)
+				continue
 			}
 
 			if curr.FsType != "swap" || curr.Type != BlockDeviceTypeCrypt {
