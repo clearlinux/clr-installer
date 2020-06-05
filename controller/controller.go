f3b943149c10d500f205e7a95db10850d8b0c90c
Mark D Horn <mark.d.horn@intel.com>
2020-06-05 11:30:21 -0700

    controller: Fix hang bug for cbm fails
    Fix issue where we failed and then returned where
    we waited on the fail again.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- controller/controller.go
+++ controller/controller.go
@@ -664,7 +664,6 @@ func contentInstall(rootDir string, version string,
 		if err := sw.DisableUpdate(); err != nil {
 			warnMsg := utils.Locale.Get("Disabling automatic updates failed")
 			log.Warning(warnMsg)
-			prg.Failure()
 			return prg, err
 		}
 		prg.Success()
@@ -696,7 +695,6 @@ func contentInstall(rootDir string, version string,
 
 	err := cmd.RunAndLogWithEnv(envVars, args...)
 	if err != nil {
-		prg.Failure()
 		return prg, errors.Wrap(err)
 	}
 	prg.Success()
